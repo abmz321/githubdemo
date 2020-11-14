bb91eca8b31b6ff9ca0882b9cb35732baf194bd4
liqiang <1020372814@qq.com>
2020-11-14 17:27:36 +0800

    添加狗类

---
+++ src/com/bjpowernode/github/bean/Dog.java
@@ -0,0 +1,19 @@
+package com.bjpowernode.github.bean;
+
+/**
+ * @ProjectName: githubDemo01
+ * @Package: com.bjpowernode.github.bean
+ * @Description: java类作用描述
+ * @Author: 李强
+ * @CreateDate: 2020/11/14 0014 17:12
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Dog {
+
+    String jum;
+    int age;
+    String sex;
+}
+
